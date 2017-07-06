748541f5d4d8568a084ed7ae1eff93bac640ce73
Gustavo Serra <g.serra@ihealthlabs.com>
2017-07-06 12:28:06 +0200

    Adding CareKit and ResearchKit frameworks. Adding MainTabViewController with 3 empty ViewControllers. Adding utilities classes. Adding icons.

---
+++ MainTabViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MainTabViewController.swift
+//  Zombie Apocalypse
+//
+//  Created by Gustavo Serra on 06/07/2017.
+//  Copyright © 2017 Gustavo Serra. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class MainTabViewController: UITabBarController {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let careCardStack = createCareCardStack()
+        let insightsStack = createInsightsStack()
+        let connectStack = createConnectStack()
+        
+        self.viewControllers = [careCardStack,
+                                insightsStack,
+                                connectStack]
+        
+        tabBar.tintColor = UIColor.darkOrange()
+        tabBar.barTintColor = UIColor.lightGreen()
+    }
+
+    fileprivate func createCareCardStack() -> UINavigationController {
+        let viewController = UIViewController()
+        
+        viewController.tabBarItem = UITabBarItem(title: "Zombie Training", image: UIImage(named: "carecard"), selectedImage: UIImage(named: "carecard-filled"))
+        viewController.title = "Zombie Training"
+        return UINavigationController(rootViewController: viewController)
+    }
+    
+    fileprivate func createInsightsStack() -> UINavigationController {
+        let viewController = UIViewController()
+        
+        viewController.tabBarItem = UITabBarItem(title: "Insights", image: UIImage(named: "insights"), selectedImage: UIImage.init(named: "insights-filled"))
+        viewController.title = "Insights"
+        return UINavigationController(rootViewController: viewController)
+    }
+    
+    fileprivate func createConnectStack() -> UINavigationController {
+        let viewController = UIViewController()
+        
+        viewController.tabBarItem = UITabBarItem(title: "Connect", image: UIImage(named: "connect"), selectedImage: UIImage.init(named: "connect-filled"))
+        viewController.title = "Connect"
+        return UINavigationController(rootViewController: viewController)
+    }
+
+}
